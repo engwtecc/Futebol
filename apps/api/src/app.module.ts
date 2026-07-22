b7e9ecd6a2ae680247e0fca75c63a3dd6daa9412
engwtecc <engenharia@wtecc.com.br>
2026-07-22 15:05:40 -0300

    Refactor AppModule to remove PlayersModule import

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,12 +1,9 @@
+import { Module } from '@nestjs/common';
+import { PrismaModule } from './prisma/prisma.module';
+
 @Module({
-
-    imports:[
-
-        PrismaModule,
-
-        PlayersModule
-
-    ]
-
+  imports: [
+    PrismaModule
+  ]
 })
-export class AppModule{}
+export class AppModule {}
