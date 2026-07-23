abac9f57010131adc48b89e7969c34fc6517a6fa
engwtecc <engenharia@wtecc.com.br>
2026-07-23 08:57:41 -0300

    Update main.ts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,15 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  console.log('Iniciando aplicação...');
 
-    const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule);
 
-    app.enableCors();
+  app.enableCors();
 
-    await app.listen(process.env.PORT || 3000);
+  const port = Number(process.env.PORT) || 3000;
 
-    console.log(`🚀 API rodando na porta ${process.env.PORT || 3000}`);
+  console.log('PORT =', process.env.PORT);
 
+  await app.listen(port, '0.0.0.0');
+
+  console.log(`🚀 API rodando na porta ${port}`);
 }
 
 bootstrap();
