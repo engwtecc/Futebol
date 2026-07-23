2b72524b744b19b3b8844618080e1c674c4fba16
engwtecc <engenharia@wtecc.com.br>
2026-07-23 09:16:10 -0300

    Update main.ts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
-
+  app.setGlobalPrefix('api');
   const port = Number(process.env.PORT) || 3000;
 
   await app.listen(port, '0.0.0.0');
