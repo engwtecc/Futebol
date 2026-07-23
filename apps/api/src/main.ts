55a4226542e3c9fb6ed745c88c10383771e72974
engwtecc <engenharia@wtecc.com.br>
2026-07-23 09:10:22 -0300

    Update main.ts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -10,8 +10,6 @@ async function bootstrap() {
 
   const port = Number(process.env.PORT) || 3000;
 
-  console.log('PORT =', process.env.PORT);
-
   await app.listen(port, '0.0.0.0');
 
   console.log(`🚀 API rodando na porta ${port}`);
